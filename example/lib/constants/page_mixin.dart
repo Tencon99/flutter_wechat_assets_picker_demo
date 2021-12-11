70365913b6a5ca7e6008f294c7db5460ccc05bba
Tencon99 <cyhtq1999@163.com>
2021-12-12 03:40:13 +0800

    add:选中图片资源后压缩图片

--- example/lib/constants/page_mixin.dart
+++ example/lib/constants/page_mixin.dart
@@ -2,13 +2,19 @@
 /// [Author] Alex (https://github.com/AlexV525)
 /// [Date] 2021/7/13 11:46
 ///
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart'
     show
         AssetEntity,
         DefaultAssetPickerProvider,
         DefaultAssetPickerBuilderDelegate;
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 import '../widgets/method_list_view.dart';
 import '../widgets/selected_assets_list_view.dart';
@@ -40,6 +46,22 @@ mixin ExamplePageMixin<T extends StatefulWidget> on State<T> {
     final List<AssetEntity>? result = await model.method(context, assets);
     if (result != null) {
       assets = List<AssetEntity>.from(result);
+      // 对选中图片进行压缩
+      for (final AssetEntity item in assets) {
+        if(item.type == AssetType.image){
+          final File? file = await item.originFile; 
+          final Directory _temp = await getTemporaryDirectory();
+          final String _path = _temp.path;
+          // 将要压缩的文件本地路径
+          final String path =  file!.path;
+          // 将要压缩的文件名称
+          final String name = path.substring(path.lastIndexOf('/') +1,path.length);//todo---> name =  uuid+ '_' + name
+          FlutterImageCompress.compressAndGetFile(path,'$_path/img_$name',quality: 50).then((File? value) {
+            log('压缩前的原文件路径、大小----> 路径：${file.path} 原文件大小--->${file.lengthSync()/1024/1024}M');
+            log('压缩后的文件路径、大小----> 路径：${value!.path} 文件大小--->${value.lengthSync()/1024/1024}M');
+          });
+        }
+      }
       if (mounted) {
         setState(() {});
       }
@@ -54,7 +76,7 @@ mixin ExamplePageMixin<T extends StatefulWidget> on State<T> {
     setState(() {});
   }
 
-  void onResult(List<AssetEntity>? result) {
+  Future<void> onResult(List<AssetEntity>? result) async {
     if (result != null && result != assets) {
       assets = List<AssetEntity>.from(result);
       if (mounted) {
